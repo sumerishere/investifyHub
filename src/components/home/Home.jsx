ee2e4c3ea2850b0dbe0f5324116484adbf15dbcb
Pathan Sumer Khan <ksumer597@gmail.com>
2024-03-26 03:47:09 +0530

    carousel in-process

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -2,6 +2,67 @@ import React from "react";
 import FundingComp from "../FundingRules/FundingComp";
 import ScrollerBar from "../scrollerLoop/ScrollerBar";
 import "./home.css";
+// import { useState, useEffect } from "react";
+
+// function Home() {
+
+//   const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+//   const images = [
+//     "/homeImage/pexels-1990x910__.jpg",
+//     "/homeImage/business-8577370_1920.jpg"
+//     // Add more image URLs here
+//   ];
+
+//   useEffect(() => {
+//     const intervalId = setInterval(() => {
+//       setCurrentImageIndex((prevIndex) =>
+//         prevIndex === images.length - 1 ? 0 : prevIndex + 1
+//       );
+//     }, 2000); // Change image every 5 seconds
+
+//     return () => clearInterval(intervalId);
+//   }, [images.length]);
+
+//   return (
+//     <div className="mcont">
+//       <div className="root-div">
+//         <div className="child-div">
+//           <div className="grand-child-div">
+//             <div className="text-overlay">
+//               <p id="mid-text">
+//                 One of those platforms letting you to work with folks who know,
+//                 love, and support you is investifyHub📈
+//               </p>
+//             </div>
+//             {images.map((imageUrl, index) => (
+//               <img
+//                 key={index}
+//                 style={{
+//                   // width: "100%",
+//                   opacity: index === currentImageIndex ? 1 : 0,
+//                   transition: "opacity 1s ease-in-out",
+//                   position: "relative",
+//                   // top: 0, // Adjust position as needed
+//                   zIndex: index === currentImageIndex ? 1 : 0,
+//                 }}
+
+//                 src={imageUrl}
+//                 alt={`img ${index}`}
+//               />
+//             ))}
+//           </div>
+//         </div>
+//       </div>
+//       <ScrollerBar />
+//       <FundingComp />
+//     </div>
+//   );
+// }
+
+// export default Home;
+
+
 function Home() {
   return (
     <div className="mcont">
@@ -14,8 +75,6 @@ function Home() {
                 love, and support you is investifyHub📈
               </p>
             </div>
-
-            {/* <p className="mid-text">"One of those platforms letting you to work with folks who know, love, and support you is investifyHub📈"</p> */}
 
             <img
               style={{ width: "100%", height: "100%" }}
